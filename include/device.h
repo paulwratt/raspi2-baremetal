cf8d02eed51fd64712f6fdba65d0df756386381e
Ronde <none>
2015-12-06 23:38:26 +0900

    Update

---
+++ include/device.h
@@ -0,0 +1,18 @@
+#ifndef		__INCLUDE_DEVICE_H__
+#define		__INCLUDE_DEVICE_H__
+
+#include	<register.h>
+#include	<type.h>
+
+struct DEVICE_DATA
+{
+	char *name;
+	struct REG_DATA	reg;
+
+};
+
+
+
+
+#endif		//__INCLUDE_DEVICE_H__
+
